7f856eeeb29244e6bd65827286b5d3bb76f59bab
shreenapatel <shreena88@gmail.com>
2023-01-27 12:43:22 -0800

    adding script for end-to-end testing, which sends batch of requests to service

---
+++ week3/project/e2e_test.py
@@ -0,0 +1,14 @@
+"""
+Script to send a batch of requests to web app, hosted in docker container
+"""
+
+import requests
+import json
+
+examples = []
+with open('data/requests.json', 'r') as f:
+    for line in f:
+        examples.append(json.loads(line))
+
+for example in examples:
+    r = requests.post('http://0.0.0.0/predict', data=json.dumps(example))
